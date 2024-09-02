6ef1238e42e669394438a484e69bbc0f32883e1f
Ovidiu Gusteritan <gusteritanovidiu@gmail.com>
2024-09-02 16:20:43 +0300

    done alerts

--- functions.js
+++ functions.js
@@ -1,10 +1,13 @@
-export function createHome() {
+export function createHome(alert) {
 
     let container = document.querySelector(".container");
 
 
     container.innerHTML = `
-    
+    <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+       </div> 
+
     	<h1>Students</h1>
 
     <button class="button">Add student</button>
@@ -22,22 +25,76 @@ export function createHome() {
 		</tbody>
 	</table>
     `
+    let button = document.querySelector(".button");
+    let table = document.querySelector(".table");
+    const alertPlaceholder = document.querySelector('.container-alert');
+    let load = document.querySelector(".spinner-border");
+
+    const appendAlert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+            `   <div>${message}</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+        ].join('')
+
+        alertPlaceholder.append(wrapper)
+    }
 
     api("https://localhost:7222/api/v1/Student/all").then(response => {
         return response.json();
     }).then(data => {
+        load.classList = "";
         console.log(data);
         attachStudents(data.studentList);
     }).catch(error => {
+        load.classList = "";
         console.error('Error fetching data:', error);
+        appendAlert(error, "danger");
     });
-
-
-    let button = document.querySelector(".button");
 
     button.addEventListener("click", (eve) => {
         CreateAddStudentPage();
     });
+
+    table.addEventListener("click", (eve) => {
+
+        if (eve.target.classList.contains("updateStudent")) {
+            api(`https://localhost:7222/api/v1/Student/id/${eve.target.textContent}`).then(res => {
+                return res.json();
+            }).then(data => {
+                console.log(data);
+
+                let student = {
+                    name: data.name,
+                    age: data.age,
+                    specialization: data.specialization
+                }
+
+                CreateUpdatePage(student, eve.target.textContent);
+
+            }).catch(error => {
+                console.error('Error fetching data:', error);
+            });
+        }
+
+    });
+
+    if (alert === "deleted") {
+        load.classList = "";
+        appendAlert("Student has been DELETED with success!", "success");
+    }
+
+    if (alert === "updated") {
+        load.classList = "";
+        appendAlert("Student has been UPDATED with success!", "success");
+    }
+
+    if (alert === "added") {
+        load.classList = "";
+        appendAlert("Student has been ADDED with success!", "success");
+    }
 
 }
 
@@ -79,12 +136,82 @@ export function CreateAddStudentPage() {
     let test = document.querySelector(".createStudent");
 
     button.addEventListener("click", (eve) => {
-        createHome();
+        createHome("");
     })
 
     test.addEventListener("click", (eve) => {
-        createStudent();
+        createUpdateStudent("create");
     })
+
+}
+
+export function CreateUpdatePage(student, idStudent) {
+
+    let container = document.querySelector(".container");
+
+    container.innerHTML = `
+    <h1>Update Student</h1>
+    <form>
+        <p>
+            <label for="name">Name</label>
+            <input name="name" type="text" id="name" value="${student.name}">
+             <a class="nameErr">Name required!</a>
+        </p>
+        <p>
+            <label for="age">Age</label>
+            <input name="age" type="text" id="age" value="${student.age}">
+             <a class="ageErr">Age required!</a>
+        </p>
+        <p>
+            <label for="specialization">Specialization</label>
+            <input name="specialization" type="text" id="specialization" value="${student.specialization}">
+             <a class="specializationErr">Specialization required!</a>
+        </p>
+
+        <div class="submitUpdate">
+         <a href="#">Update Student</a>
+        </div>
+
+          <div class="cancel">
+         <a href="#">Cancel</a>
+        </div>
+        <div class="submitDelete">
+         <a href="#">Delete Student</a>
+        </div>
+    </form>
+    `
+
+    let cancelButton = document.querySelector(".cancel");
+    let submitUpdateButton = document.querySelector(".submitUpdate");
+    let submitDeleteButton = document.querySelector(".submitDelete");
+    let nameinput = document.getElementById("name");
+
+    nameinput.disabled = true;
+
+    cancelButton.addEventListener("click", (eve) => {
+        createHome("");
+    });
+
+    submitUpdateButton.addEventListener("click", (eve) => {
+        createUpdateStudent("update", idStudent);
+    });
+
+    submitDeleteButton.addEventListener("click", (eve) => {
+
+        api(`https://localhost:7222/api/v1/Student/delete/${idStudent}`, "DELETE")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                createHome("deleted");
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+
+    })
+
 
 }
 
@@ -93,7 +220,7 @@ function createRow(student) {
     let tr = document.createElement("tr");
 
     tr.innerHTML = `
-				<td>${student.id}</td>
+				<td class="updateStudent">${student.id}</td>
 				<td>${student.name}</td>
 				<td>${student.age}</td>
 				<td>${student.specialization}</td>
@@ -134,7 +261,7 @@ function attachStudents(students) {
 
 }
 
-function createStudent() {
+function createUpdateStudent(request, idStudent) {
 
     const isNumber = (str) => {
         return /^[+-]?\d+(\.\d+)?$/.test(str);
@@ -203,17 +330,31 @@ function createStudent() {
             specialization: specialization
         }
 
-        api("https://localhost:7222/api/v1/Student/create", "POST", student)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                createHome();
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
+        if (request === "create") {
+            api("https://localhost:7222/api/v1/Student/create", "POST", student)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    createHome("added");
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        } else if (request === "update") {
+            api(`https://localhost:7222/api/v1/Student/update/${idStudent}`, "PUT", student)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    createHome("updated");
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
     } else {
 
         errors.forEach(err => {
